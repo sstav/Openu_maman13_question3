d2e453359b9bc68e7aa148360f1a9ce617bf6a69
Stav Magalnik <ststav@gmail.com>
2021-08-22 17:33:13 +0300

    Update Classes of Users

--- meta.py
+++ meta.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Standard import
+import importlib
 
 
 class Users:
@@ -10,6 +8,7 @@ class Users:
         self.profession = profession
 
 
+# Professions Types:
 class Engineers(Users):
     def __init__(self, name, profession):
         super().__init__(name, profession)
@@ -18,7 +17,7 @@ class Engineers(Users):
         return "מהנדסים"
 
 
-class Technicians(Users):
+class Technician(Users):
     def __init__(self, name, profession):
         super().__init__(name, profession)
 
@@ -34,10 +33,64 @@ class Barber(Users):
         return "ספרים"
 
 
-class Politicians(Users):
+class Politician(Users):
     def __init__(self, name, profession):
         super().__init__(name, profession)
 
     def __str__(self):
         return "פוליטיקאים"
+
+
+# Types Of Engineers:
+# Electrical:
+class Electrical(Engineers):
+    def __init__(self, name, profession):
+        super().__init__(name, profession)
+
+    def __str__(self):
+        return super().__str__() + ": " + "חשמל"
+
+
+# Computers:
+class Computers(Engineers):
+    def __init__(self, name, profession):
+        super().__init__(name, profession)
+
+    def __str__(self):
+        return super().__str__() + ": " + "מחשבים"
+
+
+# Machines:
+class Machines(Engineers):
+    def __init__(self, name, profession):
+        super().__init__(name, profession)
+
+    def __str__(self):
+        return super().__str__() + ": " + "מכונות"
+
+
+# Helpers Functions:
+def factory(classname, name, profession):
+    # Get class from globals and create an instance
+    m = globals()[classname]("234", "234")
+    # Get the function (from the instance) that we need to call
+    func = getattr(m,"234","234")
+    # Return function
+    return func()
+
+#Main Program:
+
+
+if __name__ == "__main__":
+    while_quit = True
+    while while_quit:
+        new_class = input("Please enter the name of new class:")
+        base_class = input("Please enter name of base class (blank if none):")
+        new_method = input("Please enter name of new method for class Student:")
+        new_attr = input("Please enter name of new attribute for class Student:")
+
+        res = factory(res, 2323, 2323)
+        print(res)
+
+
 
